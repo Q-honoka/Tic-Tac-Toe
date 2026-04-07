2dd36f48aa114d57c5e3847a611b54792ee6c1b9
cre <cre@602-33>
2026-04-07 13:45:22 +0900

    プロジェクトファイルを追加。

---
+++ Tic-Tac-Toe/main.cpp
@@ -0,0 +1,25 @@
+﻿#include "DxLib.h"
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+{
+	ChangeWindowMode(TRUE);
+	SetGraphMode(640, 480, 32);
+	SetDrawScreen(DX_SCREEN_BACK);
+	
+	if (DxLib_Init() == -1)
+	{
+		return -1;
+	}
+
+	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
+	{
+		ClearDrawScreen();
+
+		DrawPixel(320, 240, GetColor(255, 255, 255));
+
+		ScreenFlip();
+	}
+
+	DxLib_End();
+	return 0;
+}
