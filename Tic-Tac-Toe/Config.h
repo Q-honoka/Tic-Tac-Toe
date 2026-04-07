b08697df31aa74a13172ee5186e378d21447cfb6
Q-honoka <ncr2419gp0006@edu.nca.ac.jp>
2026-04-07 14:15:17 +0900

    main.cppでボードの作成と表示をした

---
+++ Tic-Tac-Toe/Config.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+namespace Config
+{
+	// ウィンドウ設定
+	namespace Window
+	{
+		const int width = 640;
+		const int height = 480;
+		const int colorBitDepth = 32;
+	}
+
+	// ボード設定
+	namespace Board
+	{
+		const int size = Config::Window::width / 4 * 2;
+		const unsigned int boardColor[3] = { 255,255,255 };
+		const unsigned int lineColor[3] = { 255,0,255 };
+	}
+}
--- Tic-Tac-Toe/main.cpp
+++ Tic-Tac-Toe/main.cpp
@@ -1,9 +1,10 @@
 ﻿#include "DxLib.h"
+#include "Config.h"
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
 	ChangeWindowMode(TRUE);
-	SetGraphMode(640, 480, 32);
+	SetGraphMode(Config::Window::width, Config::Window::height, Config::Window::colorBitDepth);
 	SetDrawScreen(DX_SCREEN_BACK);
 	
 	if (DxLib_Init() == -1)
@@ -11,11 +12,26 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, 
 		return -1;
 	}
 
+	// ボードの作成
+	int boardHandle = -1;
+	boardHandle = MakeScreen(Config::Board::size, Config::Board::size, FALSE);
+	SetDrawScreen(boardHandle);
+	DrawBox(0, 0, Config::Board::size, Config::Board::size, GetColor(Config::Board::boardColor[0], Config::Board::boardColor[1], Config::Board::boardColor[2]), TRUE);
+	for (int i = 0; i < 4; i++)
+	{
+		DrawLine(Config::Board::size / 3 * i, 0, Config::Board::size / 3 * i, Config::Board::size, GetColor(Config::Board::lineColor[0], Config::Board::lineColor[1], Config::Board::lineColor[2]), 3);
+		DrawLine(0, Config::Board::size / 3 * i, Config::Board::size, Config::Board::size / 3 * i, GetColor(Config::Board::lineColor[0], Config::Board::lineColor[1], Config::Board::lineColor[2]), 3);
+	}
+
+	// 描画先を戻す
+	SetDrawScreen(DX_SCREEN_BACK);
+
 	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
 	{
 		ClearDrawScreen();
 
-		DrawPixel(320, 240, GetColor(255, 255, 255));
+		// ボードの表示
+		DrawGraph(Config::Window::width / 4, Config::Window::height / 4, boardHandle, TRUE);
 
 		ScreenFlip();
 	}
